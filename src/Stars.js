ed170a2c39c61212311f85e3046555bb8d4f1fd2
Oliver Williams <oliver.williams@springernature.com>
2018-12-14 11:08:19 +0000

    turn into bar chart

--- src/Stars.js
+++ src/Stars.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import Star from './Star';
+import { ResponsiveBar } from '@nivo/bar';
 
 const query = gql`
 fragment fields on Repository {
@@ -32,12 +32,20 @@ fela: repository(owner: "rofrischmann", name: "fela") {
 styledjsx: repository(owner: "zeit", name: "styled-jsx") {
   ...fields
 } 
-  styletron: repository(owner: "styletron", name: "styletron") {
+styletron: repository(owner: "styletron", name: "styletron") {
+  ...fields
+}
+radium: repository(owner: "FormidableLabs", name: "radium") {
+  ...fields
+} 
+glamorous: repository(owner: "paypal", name: "glamorous") {
+  ...fields
+} 
+glamor: repository(owner: "threepointone", name: "glamor") {
   ...fields
 } 
 }
 `
-
 
 const Stars = () => (
 	<Query query = {query}>
@@ -45,14 +53,62 @@ const Stars = () => (
 {({loading, error, data}) => 
 {
 	 const sortedData = Object.keys(data).sort((a, b) => data[b].stargazers.totalCount - data[a].stargazers.totalCount );
-	
-	 return sortedData.map((key, index) => {
-		const {name, description, url, stargazers} = data[key];
-		return <div>
-			<a href={url}><Star>{stargazers.totalCount}</Star></a>
-			<div>{name}</div>
-			</div>
-	})
+
+	 let starsData = [];
+	 sortedData.forEach(repoName => {
+		 starsData.push({name: repoName, stars: data[repoName].stargazers.totalCount})
+	 });
+
+
+	 console.log(starsData);
+
+	 return (<ResponsiveBar
+	 data={starsData}
+	 keys={["stars"]}
+	 indexBy="name"
+	 margin={{
+		 "top": 50,
+		 "right": 130,
+		 "bottom": 50,
+		 "left": 60
+	 }}
+	 padding={0.3}
+	 layout="horizontal"
+	 borderColor="inherit:darker(1.6)"
+	 axisBottom={{
+		 "tickSize": 5,
+		 "tickPadding": 5,
+		 "tickRotation": 0,
+		 "legend": "Github stars",
+		 "legendPosition": "middle",
+		 "legendOffset": 32
+	 }}
+	 
+	 legends={[
+		 {
+			 "dataFrom": "keys",
+			 "anchor": "bottom-right",
+			 "direction": "column",
+			 "justify": false,
+			 "translateX": 120,
+			 "translateY": 0,
+			 "itemsSpacing": 2,
+			 "itemWidth": 100,
+			 "itemHeight": 30,
+			 "itemDirection": "left-to-right",
+			 "itemOpacity": 0.85,
+			 "symbolSize": 20,
+			 "effects": [
+				 {
+					 "on": "hover",
+					 "style": {
+						 "itemOpacity": 1
+					 }
+				 }
+			 ]
+		 }
+	 ]}
+	 />)
 }
 }
 
