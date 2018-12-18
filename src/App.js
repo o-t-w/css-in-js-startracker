ab62ca4747c99b4159ba680346097b427b74ecca
Oliver Williams <oliver.williams@springernature.com>
2018-12-18 11:58:12 +0000

    hardcode data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo';
 import Stars from './Stars';
-
-const client = new ApolloClient({
-	uri: "https://api.github.com/graphql",
-	headers: {Authorization: "bearer f580cecfa83440a42e934978a0020723463e10fc"}
-});
-
-
 
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
 		<div className="App">
 		<Stars/>
       	</div>
-	  </ApolloProvider>
     );
   }
 }
--- src/Stars.js
+++ src/Stars.js
@@ -1,95 +1,55 @@
 import React from 'react';
-import {Query } from 'react-apollo';
-import gql from 'graphql-tag';
 import { ResponsiveBar } from '@nivo/bar';
 
-const query = gql`
-fragment fields on Repository {
-name 
-stargazers {
-	totalCount
-  }
+const data = [
+	{name: "Styled Components",
+	stars: 20967},
+	{name: "Radium",
+	stars: 6716},
+	{name: "Emotion",
+	stars: 6246},
+	{name: "Aphrodite",
+	stars: 4460},
+	{name: "JSS",
+	stars: 4380},
+	{name: "Styled JSX",
+	stars: 3901},
+	{name: "Glamorous",
+	stars: 3774},
+	{name: "Glamor",
+	stars: 3375},
+	{name: "Styletron",
+	stars: 2622},
+	{name: "Fela",
+	stars: 1504},
+]
+
+const Stars = () => {
+	return <ResponsiveBar
+	data={data}
+	keys={["stars"]}
+	indexBy="name"
+	margin={{
+		"top": 40,
+		"right": 20,
+		"bottom": 60,
+		"left": 120
+	}}
+	padding={0.3}
+	layout="horizontal"
+	borderColor="inherit:darker(1.6)"
+	axisBottom={{
+		"tickSize": 4,
+		"tickPadding": 3,
+		"tickRotation": 0,
+		"tickValues": [0, 10000, 20000],
+		"legend": "Github stars",
+		"legendPosition": "middle",
+		"legendOffset": 36
+	}}
+	
+	/>
+
 }
-
-query {
-styledComponents: repository(owner: "styled-components", name: "styled-components") {
-...fields
-} 
-emotion: repository(owner: "emotion-js", name: "emotion") {
- ...fields
-} 
-aphrodite: repository(owner: "khan", name: "aphrodite") {
-  ...fields
-} 
-jss: repository(owner: "cssinjs", name: "jss") {
-		...fields
-} 
-fela: repository(owner: "rofrischmann", name: "fela") {
-  ...fields
-} 
-styledjsx: repository(owner: "zeit", name: "styled-jsx") {
-  ...fields
-} 
-styletron: repository(owner: "styletron", name: "styletron") {
-  ...fields
-}
-radium: repository(owner: "FormidableLabs", name: "radium") {
-  ...fields
-} 
-glamorous: repository(owner: "paypal", name: "glamorous") {
-  ...fields
-} 
-glamor: repository(owner: "threepointone", name: "glamor") {
-  ...fields
-} 
-}
-`
-
-const Stars = () => (
-	<Query query = {query}>
-
-{({loading, error, data}) => 
-{
-	if (loading) return <p>Loading data...</p>
-	if (error) return <p>Error :(</p>
-	 const sortedData = Object.keys(data).sort((a, b) => data[b].stargazers.totalCount - data[a].stargazers.totalCount );
-
-	 let starsData = [];
-	 sortedData.forEach(repoName => {
-		 starsData.push({name: data[repoName].name, stars: data[repoName].stargazers.totalCount})
-	 });
-
-
-	 console.log(starsData);
-
-	 return (<ResponsiveBar
-	 data={starsData}
-	 keys={["stars"]}
-	 indexBy="name"
-	 margin={{
-		 "top": 40,
-		 "right": 20,
-		 "bottom": 60,
-		 "left": 120
-	 }}
-	 padding={0.3}
-	 layout="horizontal"
-	 borderColor="inherit:darker(1.6)"
-	 axisBottom={{
-		 "tickSize": 4,
-		 "tickPadding": 3,
-		 "tickRotation": 0,
-		 "tickValues": [0, 10000, 20000],
-		 "legend": "Github stars",
-		 "legendPosition": "middle",
-		 "legendOffset": 36
-	 }}
-	 
-	 />)
-}
-}
-
-</Query>
-)
 
 export default Stars;
